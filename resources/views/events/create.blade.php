3dcdf1c8f173d31db175dc2ca8a6596fb42938ef
DChivela <domingostchivela18@gmail.com>
2024-07-24 17:37:22 +0100

    Trabalhando no CREATE

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
 
-<h1>Crie um evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="post"> <!-- Acção após clicar no botão para salvar -->
+        <div class="form-group">
+
+        </div>
+    </form>
+</div>
 
 @endsection
