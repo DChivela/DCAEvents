717df4b00787fd8b4c89a2542b2987a849b4dc5d
DChivela <domingostchivela18@gmail.com>
2024-07-25 09:46:16 +0100

    Actualização do formulário e verificação do ficheiro route

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -6,22 +6,25 @@
 
 <div id="event-create-container" class="col-md-6 offset-md-3">
     <h1>Crie o seu evento</h1>
-    <form action="/events" method="post"> <!-- Acção após clicar no botão para salvar -->
-        <!-- Primeiro Campo - Título -->
+    <form action="/events" method="POST"> <!-- Acção após clicar no botão para salvar -->
+        <!-- Protecção do Laravel -->
+     @csrf 
+
+        <!-- Primeiro Campo - Título --> 
         <div class="form-group">
             <label for="title">Evento:</label>
-            <input type="text" class="form-control" id="title" placeholder="Nome do evento">
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
         </div>
-
+        
         <!-- Segundo Campo - Cidade -->
         <div class="form-group">
-            <label for="title">Cidade:</label>
-            <input type="text" class="form-control" id="city" placeholder="Local do evento">
+            <label for="city">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
         </div>
 
         <!-- Terceiro Campo - Descrição -->
         <div class="form-group">
-            <label for="title">Privacidade do evento:</label>
+            <label for="private">Privacidade do evento:</label>
             <select name="private" id="private" class="form-control">
                 <option value="0">Não</option>
                 <option value="1">Sim</option>
@@ -30,7 +33,7 @@
 
         <!-- Quarto Campo - Descrição -->
         <div class="form-group">
-            <label for="title">Descrição do Evento:</label>
+            <label for="description">Descrição do Evento:</label>
             <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
         </div>
         <input type="submit" class="btn btn-primary" value="Criar Evento">
--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,5 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/', [EventController::class, 'store']);
 
-Route::get('/produtos', function () {
-
-    $busca = request('search');
-    return view('products', ['busca' => $busca]);
-});
-
-//Paramêtro na rota esperando o ID
-Route::get('/produto/{id}', function ($id) {
-    return view('product', ['id' => $id]);
-});
-
-//Paramêtro na rota esperando o ID
-Route::get('/produt/{id?}', function ($id = null ) {
-    return view('product', ['id' => $id]);
-});
