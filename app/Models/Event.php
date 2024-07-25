5bc8581e5dae4d7b7f0d500e2184c02c6112969d
DChivela <domingostchivela18@gmail.com>
2024-07-25 16:46:21 +0100

    Conclusão da página SHOW DETAILS

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+    //Sintaxe propícia do Laravel que usamos para indicar ao sistema que esse dado é um array e não um String
+    protected $casts = [
+        'items' => 'array'
+    ];
 }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -8,20 +8,20 @@
     <h1>Crie o seu evento</h1>
     <form action="/events" method="POST" enctype="multipart/form-data"> <!-- Acção após clicar no botão para salvar -->
         <!-- Protecção do Laravel -->
-     @csrf 
+        @csrf
 
-     <!-- Campo do File --> 
-     <div class="form-group">
+        <!-- Campo do File -->
+        <div class="form-group">
             <label for="image">Imagem do Evento:</label>
             <input type="file" class="form-control-file" id="image" name="image">
         </div>
 
-        <!-- Primeiro Campo - Título --> 
+        <!-- Primeiro Campo - Título -->
         <div class="form-group">
             <label for="title">Evento:</label>
             <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
         </div>
-        
+
         <!-- Segundo Campo - Cidade -->
         <div class="form-group">
             <label for="city">Cidade:</label>
@@ -41,6 +41,26 @@
         <div class="form-group">
             <label for="description">Descrição do Evento:</label>
             <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+        </div>
+
+        <!-- Checkbox - Usando Json -->
+        <div class="form-group">
+            <label for="description">Adicione itens de infraestrutura:</label>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Palco"> Palco
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cerveja"> Cerveja
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Fast Food"> Fast Food
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Pipocas"> Pipocas
+            </div>
         </div>
         <input type="submit" class="btn btn-primary" value="Criar Evento">
     </form>
